8399def2ea7805fcc7175b96197093dd07f996de
VenimirPetkov <venimir@fusionlab.uk>
2024-04-26 12:44:23 +0200

    downgrade pragma because of push0

--- libraries/Strings.sol
+++ libraries/Strings.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.0.0) (utils/Strings.sol)
 
-pragma solidity 0.8.20;
+pragma solidity 0.8.19;
 
 import { Math } from "./Math.sol";
 import { SignedMath } from "./SignedMath.sol";
