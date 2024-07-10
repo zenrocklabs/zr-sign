cdcefcd307e22bb9ca769548ba9921a60498940d
VenimirPetkov <venimir@fusionlab.uk>
2024-07-10 16:37:01 +0300

    deploy script added

--- ignition/modules/zr-sign-init.ts
+++ ignition/modules/zr-sign-init.ts
@@ -13,17 +13,11 @@ const chainIdOptimismSepolia = "eip155:11155420";
 const mpcAddress = "0xF9F59E34fe863918be62EE585364Eb46ed7142DD";
 
 export default buildModule("ZrSignInit", (m) => {
-    console.log("Starting ZrSignInit module initialization...");
-
     const { ZrSignTypes, ZrSignImpl, ZrSignProxy } = m.useModule(ZrSignModule);
-    console.log("Retrieved ZrSign module components.");
 
-    console.log("Configuring wallet type ID...");
     const callWalletTypeIdConfig = m.call(ZrSignProxy, "walletTypeIdConfig", [44, 60, true]);
-    console.log("Wallet type ID configured.");
 
     // Chain setup
-    console.log("Configuring chain IDs...");
     m.call(ZrSignProxy, "chainIdConfig", [evmWalletTypeId, chainIdSepolia, true], {
         id: "chainConfigSepolia",
         after: [callWalletTypeIdConfig],
@@ -56,41 +50,29 @@ export default buildModule("ZrSignInit", (m) => {
         id: "chainConfigOptimismSepolia",
         after: [callWalletTypeIdConfig],
     });
-    console.log("Chain IDs configured.");
 
     // Role setup
-    console.log("Granting MPC role...");
     const mpcRole = m.staticCall(ZrSignProxy, "MPC_ROLE");
     m.call(ZrSignProxy, "grantRole", [mpcRole, mpcAddress]);
     console.log("MPC role granted.");
 
-    console.log("Granting tokenomics role...");
     const tokenomicsRole = m.staticCall(ZrSignProxy, "FEE_ROLE");
     const grantRoleTokenomics = m.call(ZrSignProxy, "grantRole", [tokenomicsRole, m.getAccount(3)], {
         id: "grantRole_tokenomics"
     });
 
-    console.log("Setting up base fee...");
     m.call(ZrSignProxy, "updateMPCFee", [500000000000000], {
         from: m.getAccount(3),
         after: [grantRoleTokenomics],
     });
-    console.log("Base fee set up.");
-
-    console.log("Setting up response gas...");
     m.call(ZrSignProxy, "updateRespGas", [200000], {
         from: m.getAccount(3),
         after: [grantRoleTokenomics],
     });
-    console.log("Response gas set up.");
-
-    console.log("Setting up response gas buffer...");
     m.call(ZrSignProxy, "updateRespGasBuffer", [120], { //120 = 20%
         from: m.getAccount(3),
         after: [grantRoleTokenomics],
     });
-    console.log("Response gas buffer set up.");
-
     console.log("ZrSignInit module initialization completed.");
 
     return { ZrSignTypes: ZrSignTypes, ZrSignImpl: ZrSignImpl, ZrSignProxy: ZrSignProxy };
