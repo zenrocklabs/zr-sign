43502316322307065a1bee052bdc7e7d1b6d13f1
VenimirPetkov <venimir@fusionlab.uk>
2024-08-28 13:20:04 +0200

    dotenv added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,9 @@ import "@nomicfoundation/hardhat-ethers";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
-import { config as conf } from "./config";
+import * as dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file
 
 const hardhatConfig: HardhatUserConfig = {
   solidity: {
@@ -13,96 +15,78 @@ const hardhatConfig: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
   networks: {
     hardhat: {
       initialBaseFeePerGas: 1000000000, // 1 Gwei
-      gasPrice: 1000000000
+      gasPrice: 1000000000,
     },
     sepolia: {
       url: infuraProvider("sepolia"),
-      accounts: hdWallet()
+      accounts: hdWallet(),
     },
     polygon_amoy: {
       url: infuraProvider("polygon-amoy"),
-      accounts: hdWallet()
+      accounts: hdWallet(),
     },
     avalanche_fuji: {
       url: infuraProvider("avalanche-fuji"),
-      accounts: hdWallet()
+      accounts: hdWallet(),
     },
     arb_sepolia: {
       url: infuraProvider("arbitrum-sepolia"),
-      accounts: hdWallet()
+      accounts: hdWallet(),
     },
     binance_testnet: {
-      url: `https://young-falling-diagram.bsc-testnet.quiknode.pro/${conf.BINANCE_RPC_KEY}/`,
-      accounts: hdWallet()
+      url: `https://young-falling-diagram.bsc-testnet.quiknode.pro/${process.env.BINANCE_RPC_KEY || ""}/`,
+      accounts: hdWallet(),
     },
     base_sepolia: {
-      url: `https://frosty-bold-owl.base-sepolia.quiknode.pro/${conf.BASE_RPC_KEY}/`,
-      accounts: hdWallet()
+      url: `https://frosty-bold-owl.base-sepolia.quiknode.pro/${process.env.BASE_RPC_KEY || ""}/`,
+      accounts: hdWallet(),
     },
     optimism_sepolia: {
       url: infuraProvider("optimism-sepolia"),
-      accounts: hdWallet()
-    }
+      accounts: hdWallet(),
+    },
   },
   etherscan: {
     apiKey: {
-      sepolia: conf.ETHERSCAN_KEY,
-      avalancheFujiTestnet: conf.AVALANCHE_KEY,
-      polygonMumbai: conf.POLYGON_KEY,
-      polygonAmoy: conf.POLYGON_KEY,
-      optimism_sepolia: conf.OPTIMISM_KEY,
-      arbitrumSepolia: conf.ARBITRUM_KEY,
-      bscTestnet: conf.BINANCE_KEY,
-      baseSepolia: conf.BASE_KEY
+      sepolia: process.env.ETHERSCAN_KEY || "",
+      avalancheFujiTestnet: process.env.AVALANCHE_KEY || "",
+      polygonMumbai: process.env.POLYGON_KEY || "",
+      polygonAmoy: process.env.POLYGON_KEY || "",
+      optimism_sepolia: process.env.OPTIMISM_KEY || "",
+      arbitrumSepolia: process.env.ARBITRUM_KEY || "",
+      bscTestnet: process.env.BINANCE_KEY || "",
+      baseSepolia: process.env.BASE_KEY || "",
     },
-    customChains: [
-      {
-        network: "optimism_sepolia",
-        chainId: 11155420,
-        urls: {
-          apiURL: "https://api-sepolia-optimistic.etherscan.io/api",
-          browserURL: "https://sepolia-optimism.etherscan.io"
-        }
-      }
-    ]
   },
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
+    artifacts: "./artifacts",
   },
   mocha: {
-    timeout: 40000
-  }
+    timeout: 40000,
+  },
 };
 
 function infuraProvider(network: string) {
-  if (!conf.INFURA_KEY) {
-    console.error("A valid INFURA_KEY must be provided in config.ts");
-    process.exit(1);
-  }
-  return `https://${network}.infura.io/v3/${conf.INFURA_KEY}`;
+  return `https://${network}.infura.io/v3/${process.env.INFURA_KEY || ""}`;
 }
 
 function hdWallet() {
-  if (!conf.MNEMONIC) {
-    console.error("A valid MNEMONIC must be provided in config.ts");
-    process.exit(1);
-  }
   return {
-    mnemonic: conf.MNEMONIC,
+    mnemonic: process.env.MNEMONIC || "",
     path: "m/44'/60'/0'/0",
     initialIndex: 0,
     count: 20,
-    passphrase: ""
+    passphrase: "",
   };
 }
 
